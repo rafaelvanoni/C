6b12d4c4e9d4db58e90eea7c6136184233edec4d
Rafael Vanoni <rafael@fsi.io>
2021-03-30 17:28:41 -0700

    strtok_r(3) example

---
+++ strtok_r.c
@@ -0,0 +1,25 @@
+/*
+ * https://github.com/rafaelvanoni/C.git
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+int
+main(int argc, char **argv)
+{
+	char foo[] = "one,two,three";
+	char *delim = ", \t\r\n";
+	char *endp = NULL;
+	char *tok;
+
+	tok = strtok_r(foo, delim, &endp);
+	while (tok != NULL) {
+		printf("%s %ld\n", tok, strlen(tok));
+		tok = strtok_r(NULL, delim, &endp);
+	}
+
+	return (0);
+}
